79d864ee3dda7a4f1d2c70c257dd4a6029b451cd
Alex Laptop <atpugh20@gmail.com>
2024-08-18 20:44:50 -0400

    start of merge sort

---
+++ C1/mergeSort.c
@@ -0,0 +1,20 @@
+/* This program takes an array of numbers and sorts them 
+ * from smallest to largest using merge sort */
+
+// INIT //
+
+#include <stdio.h>
+
+void printArray(int array[], int size);
+void sort(int 
+
+// MAIN //
+
+int main(void) {
+
+    return 0;
+}
+
+// FUNCTIONS //
+
+
