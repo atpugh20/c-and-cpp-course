fcdfbd105decf650673ed205602788249f230704
Alex PC <atpugh20@gmail.com>
2024-08-20 15:36:51 -0700

    change test for exam

--- C1/test.c
+++ C1/test.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+int mystery(int p, int q) {
+	int r;
+	if ((r = p % q) == 0) return q;
+	else return mystery(q, r);
+
+}
+
 int main() {
-  int i,j,n;
-  i = 1; j = 2;
-  n = i, j+1;
-  printf("%d", n);
+	int a[10] = {1,2,3,4,5,6,7,8,9,10}, i = 6 ;
+	int *p = &a[0];
+	printf("%d\n", *(p + i));
 }
