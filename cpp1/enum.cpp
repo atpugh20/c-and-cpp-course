68bb15335c41fda4c0a430bfd3ac20a1fe2eeca5
Alex Laptop <atpugh20@gmail.com>
2024-08-31 05:02:45 -0400

    Update enum.cpp

--- cpp1/enum.cpp
+++ cpp1/enum.cpp
@@ -1,8 +1,12 @@
-/* This program reviews enum and also an incrementing funciton */
+/* This program reviews enum and operator overloading */
 
 #include <iostream>
 
 typedef enum days {SUN, MON, TUE, WED, THU, FRI, SAT} days;
+
+inline days operator++(days d) {
+    return static_cast<days>((static_cast<int>(d) + 1) % 7);
+}
 
 int main() {
     std::cout << FRI;
