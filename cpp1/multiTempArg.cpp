33b39aad891232f11a8e0205310945fc2ce21250
Alex Laptop <atpugh20@gmail.com>
2024-08-31 03:25:38 -0400

    multiple template argument types

---
+++ cpp1/multiTempArg.cpp
@@ -0,0 +1,31 @@
+/* This program shows how templates can be used
+ * using more than one argument type */
+
+#include <iostream>
+
+template<class T1, class T2>
+
+void copy(const T1 source[], T2 destination[], int size) {
+    /* takes an array of one type, then copies 
+     * it into another array of a different type */
+
+    for (int i = 0; i < size; i++) {
+        destination[i] = static_cast<T2>(source[i]);
+    }
+
+}
+
+int main() {
+    int size = 5;
+    int nums[size] = {1,2,3,4,5};
+    double decimals[size];
+    copy(nums, decimals, size);
+
+    // print each new double to console
+    std::cout << "New array as doubles + 0.1: ";
+    for (int i = 0; i < size; i++) {
+        std::cout << decimals[i] + 0.1 << " ";
+    }
+    
+    return 0;
+}
