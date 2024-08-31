1c6e8c2f99caa4b0fe132e70fb34b5d280b4afb2
Alex Laptop <atpugh20@gmail.com>
2024-08-31 04:30:31 -0400

    enum start

---
+++ cpp1/enum.cpp
@@ -0,0 +1,10 @@
+/* This program reviews enum and also an incrementing funciton */
+
+#include <iostream>
+
+typedef enum days {SUN, MON, TUE, WED, THU, FRI, SAT} days;
+
+int main() {
+    std::cout << FRI;
+    return 0;
+}
