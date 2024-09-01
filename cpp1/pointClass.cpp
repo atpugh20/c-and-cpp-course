52e2402123a265ac402059db911a0ef43c32f7ef
Alex Laptop <atpugh20@gmail.com>
2024-08-31 20:13:41 -0400

    point class

---
+++ cpp1/pointClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Point {
+
+    public:
+        double getx() {return x;}
+        void setx(double v) {x = v;}
+    
+    private;
+        double x, y;
+
+}
+
+int main() { 
+    return 0;
+}
