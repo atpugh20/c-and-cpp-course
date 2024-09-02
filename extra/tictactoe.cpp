64d45aa8a7eaebf20591e92243a697a20c0b927f
Alex PC <atpugh20@gmail.com>
2024-09-02 02:53:20 -0700

    tictactoe2

--- extra/tictactoe.cpp
+++ extra/tictactoe.cpp
@@ -6,53 +6,87 @@
 
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 
-void clear_table(int t[]);
-void print_table(int t[]);
-int get_player_move();
-int get_computer_move();
+class Table {
+    public:
+        int size;
+        char data[9];
 
-const int T_SIZE = 9;
+        // Constructor
+        Table() {
+            size = 9;
+            clear_board();
+        }
+
+        void clear_board() {
+            /* Sets the table array back to the 
+             * default int */
+        
+            for (int i = 0; i < size; i++) {
+                data[i] = ' ';
+            }
+        }
+
+        bool check_square(int move, char player) {
+            if (data[move - 1] == ' ') {
+                data[move-1] = player;
+                return true;
+            } else {
+                std::cout << "Space filled!" << std::endl;
+                return false;
+            }
+        }
+
+        void print() {
+            /* Prints out the current tic-tac-toe table 
+             * into the console */
+
+            std::string w = "\t|\t";
+            std::string fl = "\t-------------------------------------------------";
+            std::cout << std::endl;
+            std::cout << fl << std::endl; 
+            for (int i = 0; i < size; i++) {
+                std::cout << w;
+                if (data[i] == ' ') {
+                    std::cout << "[" << i + 1 << "]";
+                } else {
+                    std::cout << data[i];
+                }
+                if ((i + 1) % 3 == 0) {
+                    std::cout << w << std::endl;
+                    std::cout << fl << std::endl;
+                }
+            }
+        }
+};
+
+int get_user_move();
+int get_cpu_move();
+void execute_turn(char player, Table& table);
 
 // MAIN //
 
 int main() {
     std::srand(std::time(NULL)); // use current time as seed
-    int table[T_SIZE];
-    clear_table(table);
-    print_table(table);
-    for (int i = 0; i < T_SIZE; i++) {
-        std::cout << table[i] << " ";
+    Table table;
+    int user_move;
+    int cpu_move;
+    const char USER = 'X';
+    const char CPU = 'O';
+    
+    // Game Loop
+    while (true) {
+        table.print();
+        execute_turn(USER, table);
+        table.print();
+        execute_turn(CPU, table);
     }
 }
 
 // FUNCTIONS //
 
-void clear_table(int t[]) {
-    /* Sets the table array back to the 
-     * default integers of 1-9 */
-    
-    for (int i = 0; i < T_SIZE; i++) {
-        t[i] = i + 1;
-    }
-}
-
-void print_table(int t[]) {
-    /* Prints out the current tic-tac-toe table 
-     * into the console */
-
-    std::string w = "\t|\t";
-    std::string fl = "\t-------------------------------------------------";
-    std::cout << std::endl;
-    for (int i = 0; i < 3; i++) {
-        int j = 3 * i; //first index of the row
-        std::cout << fl << std::endl;
-        std::cout << w << t[j] << w << t[j+1] << w << t[j+2] << w << std::endl;
-    }
-    std::cout << fl << std::endl << std::endl;
-}
-
-int get_player_move() {
+int get_user_move() {
     /* This function gets a move from 
      * the user using the console. */
     
@@ -71,9 +105,26 @@ int get_player_move() {
     }
 }
 
-int get_computer_move() {
+int get_cpu_move() {
     /* This function returns a random 
      * integer(1-9) as the computer's move */
     
     return std::rand() % 9 + 1;
+}
+
+void execute_turn(char player, Table& table) {
+    /* Starts the input player's turn and
+     * ensures that their move was successful */
+
+    int move;
+    bool move_successful;
+    
+    while (true) {
+        if (player == 'X') move = get_user_move(); 
+        else move = get_cpu_move();
+        move_successful = table.check_square(move, player);
+        if (move_successful) break;
+        else continue;
+    }
+   
 }
