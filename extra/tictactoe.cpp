7327fa2ae85da3a83f1345cefd1fe3200c48aca0
Alex PC <atpugh20@gmail.com>
2024-09-03 02:58:15 -0700

    final tictactoe

--- extra/tictactoe.cpp
+++ extra/tictactoe.cpp
@@ -2,89 +2,135 @@
  * match against the computer. The computer will 
  * choose a move at random */
 
-// INIT //
-
 #include <iostream>
 #include <ctime>
 #include <algorithm>
 
+////
+// CLASSES
+////
+
 class Table {
+    int rows, cols, last_x, last_y;
+    
     public:
-        int size;
-        char data[9];
+        int user_score, cpu_score;
+        char last_player;
+        char data[3][3];
 
         // Constructor
         Table() {
-            size = 9;
+            rows = cols = 3;
+            user_score = cpu_score = 0;
             clear_board();
         }
 
+        // Methods
+
         void clear_board() {
             /* Sets the table array back to the 
-             * default int */
+             * default char of ' ' (space) */
         
-            for (int i = 0; i < size; i++) {
-                data[i] = ' ';
-            }
-        }
-
-        bool check_square(int move, char player) {
-            if (data[move - 1] == ' ') {
-                data[move-1] = player;
-                return true;
-            } else {
-                std::cout << "Space filled!" << std::endl;
-                return false;
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    data[y][x] = ' ';
+                } 
             }
         }
 
         void print() {
-            /* Prints out the current tic-tac-toe table 
-             * into the console */
-
+            /* Prints out the current tic-tac-toe 
+             * table into the console */
+            
             std::string w = "\t|\t";
             std::string fl = "\t-------------------------------------------------";
-            std::cout << std::endl;
-            std::cout << fl << std::endl; 
-            for (int i = 0; i < size; i++) {
-                std::cout << w;
-                if (data[i] == ' ') {
-                    std::cout << "[" << i + 1 << "]";
-                } else {
-                    std::cout << data[i];
+            
+            std::cout << '\n';
+            std::cout << fl << '\n'; 
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    std::cout << w;
+                    if (data[y][x] == ' ') {
+                        int cell = y * 3 + x + 1;
+                        std::cout << cell;
+                    } else {
+                        std::cout << "[" << data[y][x] << "]";
+                    }
                 }
-                if ((i + 1) % 3 == 0) {
-                    std::cout << w << std::endl;
-                    std::cout << fl << std::endl;
+                std::cout << w << '\n';
+                std::cout << fl << '\n';
+            }
+        }
+
+        bool check_square(int move, char player) {
+            /* Checks a square to see if it filled. If not, it is filled 
+             * with the player's symbol and last move is updated */
+            
+            // convert move number to array indexes
+            int y = (move - 1) / rows;
+            int x = (move - 1) % cols;
+
+            if (data[y][x] == ' ') {
+                data[y][x] = player; // fill square
+                last_x = x; last_y = y; // update the last move
+                last_player = player;
+                return true;
+            } else {
+                if (player == 'X') std::cout << "Space filled!\n";
+                return false;
+            }
+        }
+
+        bool is_winner() {
+            /* Checks for horizontal, vertical, and 
+             * diagonal sequences in that order. */
+
+            bool is_winner = false;
+
+            if (data[last_y][0] == data[last_y][1] && data[last_y][1] == data[last_y][2]) {
+                is_winner = true;
+            } else if (data[0][last_x] == data[1][last_x] && data[1][last_x] == data[2][last_x]) {
+                is_winner = true; 
+            } else if ((last_x + last_y) % 2 == 0) {
+                if (last_x - last_y == 0) {
+                    if (data[0][0] == data[1][1] && data[1][1] == data[2][2]) {
+                        is_winner = true; 
+                    }         
+                }
+                if (last_x - last_y != 0 || last_x == 1 && last_y == 1) {
+                    if (data[2][0] == data[1][1] && data[1][1] == data[0][2]) {
+                        is_winner = true;
+                    }
                 }
             }
+            return is_winner;
+        }
+
+        bool is_draw() {
+            /* Checks every sqaure in the table for a space. 
+             * If there is a space, then it is NOT a draw  */
+            bool is_draw = true;
+            
+            for (int y = 0; y < rows; y++) {
+                for (int x = 0; x < cols; x++) {
+                    if (data[y][x] == ' ') is_draw = false;
+                }
+            }
+            return is_draw;
+        }
+
+        void print_score() {
+            /* Prints a premade scoreboard to the console */
+            std::cout << "\t-----------------\n";
+            std::cout << "\tUser Score:\t" << user_score << '\n';
+            std::cout << "\tCPU Score:\t" << cpu_score << '\n';
+            std::cout << "\t-----------------" << std::endl;
         }
 };
 
-int get_user_move();
-int get_cpu_move();
-void execute_turn(char player, Table& table);
-
-// MAIN //
-
-int main() {
-    std::srand(std::time(NULL)); // use current time as seed
-    Table table;
-    int user_move;
-    int cpu_move;
-    const char USER = 'X';
-    const char CPU = 'O';
-    
-    // Game Loop
-    while (true) {
-        table.print();
-        execute_turn(USER, table);
-        table.print();
-        execute_turn(CPU, table);
-    }
-}
-
+////
 // FUNCTIONS //
+////
 
 int get_user_move() {
     /* This function gets a move from 
@@ -92,15 +138,15 @@ int get_user_move() {
     
     while (true) {
         int move;
-        std::cout << "Please choose a square (Type '0' to quit):" << std::endl;
+        std::cout << "\n\tPlease choose a square (Type '0' to quit): ";
         std::cin >> move;
         if (move > 0 && move < 10) {
             return move;
         } else if (move == 0) {
-            std::cout << "Goodbye!" << std::endl;
+            std::cout << "\n\tGoodbye!" << std::endl;
             exit(0);
         } else {
-            std::cout << "Invalid move." << std::endl;
+            std::cout << "\n\tInvalid move." << std::endl;
         }
     }
 }
@@ -118,7 +164,6 @@ void execute_turn(char player, Table& table) {
 
     int move;
     bool move_successful;
-    
     while (true) {
         if (player == 'X') move = get_user_move(); 
         else move = get_cpu_move();
@@ -126,5 +171,70 @@ void execute_turn(char player, Table& table) {
         if (move_successful) break;
         else continue;
     }
-   
+}
+
+bool replay_prompt() {
+    /* Prompts the user to see if they want to play the game again */
+    
+    char input;
+    bool replay = true;
+    std::cout << "\n\n\tPlay again? (Y/n): ";
+    std::cin >> input;
+    if (input == 'n' || input == 'N') replay = false;
+    return replay;
+}
+
+////
+// MAIN //
+////
+
+int main() {
+    std::srand(std::time(NULL)); // use current time as seed
+    Table table;
+    int match_num = 1;
+    const char USER = 'X'; // symbol for the user
+    const char CPU = 'O'; // symbol for the CPU
+    bool playing_game = true;
+    
+    // Game Loop
+    while (playing_game) {
+        table.clear_board();
+        std::cout << "\t_________________________________________________\n\n\n";
+        std::cout << "\t- Match " << match_num << " -\n\n";
+
+        // Match Loop
+        while (true) {
+            table.print();
+
+            // User's turn
+            execute_turn(USER, table);
+            if (table.is_winner()) {
+                std::cout << "\n\t--------\n\tYOU WIN!\n\t--------\n";
+                table.user_score++;
+                break;
+            }
+
+            // Check for draw (can only happen on user's turn)
+            if (table.is_draw()) {
+                std::cout << "\n\t-----\n\tDRAW!\n\t-----\n";
+                break;
+            }
+
+            // CPU's turn
+            execute_turn(CPU, table);
+            if (table.is_winner()) {
+                table.cpu_score++;
+                std::cout << "\n\t---------\n\tYOU LOST!\n\t---------\n";
+                break;
+            }
+        }
+
+        // Post match
+        match_num++;
+        table.print();
+        table.print_score();
+        playing_game = replay_prompt(); // Ask to play again
+    }
+
+    std::cout << "\n\n\tThank you for playing!" << std::endl;
 }
