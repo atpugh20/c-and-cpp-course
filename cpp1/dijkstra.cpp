4a1fa7cdd86f2fbd2d7dace96579d6d65cc01de3
Alex PC <atpugh20@gmail.com>
2024-09-04 00:23:22 -0700

    point class

---
+++ cpp1/dijkstra.cpp
@@ -0,0 +1,9 @@
+/* This program implements the famous pathfinding 
+ * algorithm known as "Dijkstra's Algorithm." */
+
+#include <iostream>
+
+int main() {
+    
+    
+}
--- cpp1/pointClass.cpp
+++ cpp1/pointClass.cpp
@@ -1,16 +1,33 @@
+/* This program uses a point on the x and y axis
+ * to show how classes and objects are used */
+
+// Constructor methods:
+// 1. point() { x = y = 0.0; }
+// 2. point() { this->x = 0.0, this->y = 0.0; }
+// 3. point() : x(0.0), y(0.0) {}
+//
+// 3 is an initialization, which can set a const
+
+
 #include <iostream>
 
 class Point {
+    /* A point is a location on the x,y axis */
+
+    double x, y;
 
     public:
-        double getx() {return x;}
-        void setx(double v) {x = v;}
-    
-    private;
-        double x, y;
+        // constructor
+        Point(double x = 0.0, double y = 0.0) : x(x), y(y) {}
+        
+        // methods
+        double getx() { return x; }
+        double gety() { return y; }
+        void setx(double v) { x = v; }
+        void sety(double v) { y = v; }
+};
 
-}
-
-int main() { 
-    return 0;
+int main() {
+    Point a(2.1, 3.5);
+    std::cout << a.getx() << ", " << a.gety() << "\n";
 }
