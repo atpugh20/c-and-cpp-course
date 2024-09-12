24459f37c4971dc35ccebe0f8464b7f5348e021d
Alex Laptop <atpugh20@gmail.com>
2024-09-12 04:08:05 -0400

    algorithm library intro

---
+++ cpp2/algorithmLibrary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::string names[] = {"alex", "zach", "austin", "matt", "jj", "chase"};
+    for (std::string name : names) {
+        std::cout << name << '\n';
+    }
+
+    // find
+    std::string* where = find(names, names+5, "jj");
+    if (where != nullptr) std::cout << *where << '\n';
+    
+    // sort (uses quicksort)
+    sort(names, names+5);
+    for (std::string name : names) {
+        std::cout << name << '\n';
+    }
+
+    return 0;
+}
