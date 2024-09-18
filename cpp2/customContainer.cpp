a76437117c7cf8eef1321da46c61a2b8029e56b0
Alex <133706872+atpugh20@users.noreply.github.com>
2024-09-17 23:02:35 -0700

    conversion, copy, and move constructors

--- cpp2/customContainer.cpp
+++ cpp2/customContainer.cpp
@@ -8,9 +8,32 @@
 template <class T, int n>
 class my_container {
     public:
+        // Constructors
         my_container() {
             a = new T[n];
         }
+        
+        // Conversion Constructor        
+        explicit my_container(T* b):my_container() {
+            for (int i = 0; i < n; i++) {
+                a[i] = b[i];
+            }   
+        }
+        
+        // Copy Constructor
+        my_container(const my_container& b): my_container() {
+            for (int i = 0; i < n; i++) {
+                a[i] = b.a[i];
+            }
+        }
+
+        // Move Constructor
+        my_container(my_container&& b) noexcept {
+            a = b.a;
+            b.a = nullptr;
+        }
+            
+        // Deconstructor
         ~my_container() {
             delete[] a;
         }
